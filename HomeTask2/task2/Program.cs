8db4df62c88bc05cddbc39c40c71302e6f2a69c0
saidmahmud7 <boboevs7777@gmail.com>
2025-02-15 18:07:23 +0300

    solid2

---
+++ HomeTask2/task2/Program.cs
@@ -0,0 +1,31 @@
+﻿interface IProduct
+{
+    decimal GetPrice();
+}
+
+interface IDiscountableProduct : IProduct
+{
+    decimal GetDiscountPrice();
+}
+
+class Product : IDiscountableProduct
+{
+    public virtual decimal GetPrice()
+    {
+        return 100; 
+    }
+
+    public virtual decimal GetDiscountPrice()
+    {
+        return GetPrice() * 0.9m;
+    }
+}
+
+
+class DigitalProduct : IProduct
+{
+    public decimal GetPrice()
+    {
+        return 100; 
+    }
+}
---
+++ HomeTask2/task4/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+public class StudentRepository
+{
+    public virtual Student GetById(int id)
+    {
+        Console.WriteLine("get student from database.");
+        return new Student { Id = id, Name = "Ahmad" };
+    }
+
+    public virtual void Add(Student student)
+    {
+        Console.WriteLine("Adding student to database.");
+    }
+}
+
+// Cached Repository
+public class CachedStudentRepository : StudentRepository
+{
+    private readonly Dictionary<int, Student> _cache = new();
+
+    public override Student GetById(int id)
+    {
+        if (_cache.ContainsKey(id))
+        {
+            Console.WriteLine("Get student from cache.");
+            return _cache[id];
+        }
+
+        var student = base.GetById(id);
+        _cache[id] = student;
+        return student;
+    }
+
+    public override void Add(Student student)
+    {
+        throw new NotImplementedException("Cannot add students to the cache!");
+    }
+}
